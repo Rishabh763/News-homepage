76bf3e1716f187172fd12e4730d4b9aed08c92e2
Rishabh763 <rs0284388@gmail.com>
2024-01-14 17:37:55 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
+import { useLayoutEffect} from 'react';
+
 import './App.css';
 import Layout from './component/Layout';
 import Navbar from './component/Navbar';
 
 function App() {
+  useLayoutEffect(()=>{
+
+    const hamburger = document.querySelector(".hamburger");
+  const links = document.querySelector(".nav-links");
+
+  const handleClick = () => {
+    hamburger.classList.toggle("toggle");
+    links.classList.toggle("open");
+  };
+
+  if (hamburger && links) {
+    hamburger.addEventListener("click", handleClick);
+  } else {
+    console.error("Hamburger or Links not found");
+  }
+
+  return () => {
+    // Cleanup: Remove the event listener when the component unmounts
+    if (hamburger) {
+      hamburger.removeEventListener("click", handleClick);
+    }
+  };
+
+
+  },[]);
   return (
-    <main> 
+    <main > 
       <Navbar/>
       <Layout/>  
-      <div class="attribution">
+      <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
         Coded by <a href="#" target="_blank">Rishabh Singh</a>.
       </div>
